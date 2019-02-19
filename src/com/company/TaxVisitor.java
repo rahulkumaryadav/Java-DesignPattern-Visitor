47fa4dba014bacb37cabd2c6e887ab2845f19e03
rahulkumaryadav <rahulkumar96369@gmail.com>
2019-02-19 11:02:14 +0530

    visitor design pattern is commted

--- src/com/company/TaxVisitor.java
+++ src/com/company/TaxVisitor.java
@@ -12,18 +12,21 @@ public class TaxVisitor implements Visitor {
     @Override
     public double visitor(Shirt shirt) {
         System.out.println("Shirt final price with tax:");
-        return Double.parseDouble(decimalFormat.format(shirt.getPrice()*.10)+shirt.getPrice());
+        return ((shirt.getPrice()*.18)+shirt.getPrice());
     }
 
     @Override
     public double visitor(TShirt tShirt) {
         System.out.println("TShirt final price with tax:");
-        return Double.parseDouble(decimalFormat.format(tShirt.getPrice()*.18)+tShirt.getPrice());
+        //return Double.parseDouble(decimalFormat.format(tShirt.getPrice()*.18)+tShirt.getPrice());
+        return ((tShirt.getPrice()*.18)+tShirt.getPrice());
+
     }
 
     @Override
     public double visitor(Jacket jacket) {
         System.out.println("Jacket final price with tax:");
-        return Double.parseDouble(decimalFormat.format(jacket.getPrice()*.20)+jacket.getPrice());
+       // return Double.parseDouble(decimalFormat.format(jacket.getPrice()*.20)+jacket.getPrice());
+        return ((jacket.getPrice()*.18)+jacket.getPrice());
     }
 }
